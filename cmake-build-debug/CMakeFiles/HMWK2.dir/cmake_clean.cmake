afa4989ca0fd7b9baf7a276f8dcf1171f391e995
reynolj <jakereynolds@csus.edu>
2020-03-07 18:10:48 -0800

    v1.2

---
+++ cmake-build-debug/CMakeFiles/HMWK2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HMWK2.dir/main.c.o"
+  "HMWK2.exe"
+  "HMWK2.pdb"
+  "libHMWK2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HMWK2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
